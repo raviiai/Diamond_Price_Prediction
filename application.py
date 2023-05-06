0593b19583fcbc17677afd5f57bbc3a517febad5
RAVI PRAKASH YADAV <raviiprakash.ai@gmail.com>
2023-05-06 10:36:40 +0530

    version updated

--- application.py
+++ application.py
@@ -30,6 +30,8 @@ def predict_datapoint():
         predict_pipeline = PredictPipeline()
         pred = predict_pipeline.predict(final_new_data)
         results = round(pred[0], 2)
+        if results<0:
+            return render_template('error_home.html')
         return render_template('results.html' , final_result=results)
 
 
